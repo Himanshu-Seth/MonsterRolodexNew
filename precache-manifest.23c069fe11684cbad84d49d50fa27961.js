c53257a64daa00e1efbedb1fae3028ae9a72301e
Himanshu Seth <hseth9897@gmail.com>
2020-10-21 15:08:36 +0530

    Updates

---
+++ precache-manifest.23c069fe11684cbad84d49d50fa27961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5489b3be8a28838f6b2609514aad215",
+    "url": "/MonsterRolodexNew/index.html"
+  },
+  {
+    "revision": "9f3d58b937f765e46373",
+    "url": "/MonsterRolodexNew/static/css/main.cd3930bd.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/MonsterRolodexNew/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodexNew/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3d58b937f765e46373",
+    "url": "/MonsterRolodexNew/static/js/main.29b8cb31.chunk.js"
+  },
+  {
+    "revision": "1b99dca20b25b1b06aea",
+    "url": "/MonsterRolodexNew/static/js/runtime-main.3fe1571d.js"
+  }
+]);
